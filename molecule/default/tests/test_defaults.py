dfe434b355040b108e85bc3190f9928bcfedfb6c
timothylytle <timothy.lytle@miarec.com>
2022-12-28 18:05:38 +0000

    initial upload, lint errors corrected, added molecule test verifier

---
+++ molecule/default/tests/test_defaults.py
@@ -0,0 +1,22 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+def test_service(host):
+    if host.system_info.distribution == "debian":
+        s = host.service("apache2")
+    if host.system_info.distribution == "centos":
+        s = host.service("httpd")
+    assert s.is_enabled
+    assert s.is_running
+
+def test_socket(host):
+    sockets = [
+        "tcp://0.0.0.0:80",
+        "tcp://0.0.0.0:443"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
